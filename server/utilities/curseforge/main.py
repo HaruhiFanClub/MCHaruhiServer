897ee30bd7b73955fd3ad85478762711bde9e343
WakelessSloth56 <wsloth56@foxmail.com>
2020-11-30 22:22:48 +0800

    4.0.0-alpha.18

--- server/utilities/curseforge/main.py
+++ server/utilities/curseforge/main.py
@@ -1,7 +1,9 @@
 import hashlib
 import json
 import logging
+from logging import debug
 import os
+from re import T
 import sys
 
 import click
@@ -12,6 +14,7 @@ from prettytable import PrettyTable
 
 DEBUG_MODE = False
 CONFIG_FILE = ""
+MANIFEST_FILE = ""
 CONFIG = {}
 MANIFEST = {}
 
@@ -49,27 +52,28 @@ class Manifest(object):
         for index, item in enumerate(_data["minecraft"]["modLoaders"]):
             mc_tb.add_row(["→", item["id"]])
         print(mc_tb)
-        mods_tb = PrettyTable(["序号", "项目ID", "文件ID", "类型", "文件名", "SHA-1"])
+        mods_tb = PrettyTable(["序号", "项目ID", "文件ID", "类型", "文件名"])
         mods_tb.title = "模组列表"
         for index, item in enumerate(self.mods()):
             mods_tb.add_row([
                 index, item["projectID"], item["fileID"],
                 (item["type"]) if "type" in item.keys() else "-",
                 (item["name"]) if "name" in item.keys() else "-",
-                (item["hash"]) if "hash" in item.keys() else "-"
             ])
         print(mods_tb)
 
 
 @click.group(invoke_without_command=False)
-@click.option("--debug", is_flag=True,  default=False, help="启用调试模式")
+@click.option("--debug", is_flag=True, default=False, help="启用调试模式")
 @click.option("--config", "config_file", default="config.json", help="指定配置文件,默认为config.json")
 @click.option("--manifest", "manifest_file", default="manifest.json", help="指定CurseForge配置文件,默认为manifest.json")
 # @click.option("--manifest", "manifest_file", default="../../minecraft/manifest.json")
+# @click.option("--manifest", "manifest_file", default="manifest.test.json")
 def cli(debug, config_file, manifest_file):
     global DEBUG_MODE
     global CONFIG
     global CONFIG_FILE
+    global MANIFEST_FILE
     global MANIFEST
     # print(debug)
     fmt = "[%(asctime)s] %(filename)s[line:%(lineno)d] %(message)s"
@@ -85,24 +89,36 @@ def cli(debug, config_file, manifest_file):
 
     CONFIG_FILE = config_file
     CONFIG = readjson(config_file)
+    MANIFEST_FILE = manifest_file
     MANIFEST = readjson(manifest_file)
 
 
 @cli.command(help="从Manifest中获取信息")
-@ click.option("--markdown", "gen_md", is_flag=True, default=False, help="")
-def info(gen_md):
+# @ click.option("--markdown", "gen_md", is_flag=True, default=False, help="")
+def info():
     logging.debug(json.dumps(MANIFEST, ensure_ascii=False, indent=4))
     mObj = Manifest(MANIFEST)
     mObj.info_table()
 
 
+@cli.command(help="格式化Manifest文件")
+@click.option("--sort-mods", "sortmods", is_flag=True, default=False, help="根据Mod的Project ID排序files列表")
+def format(sortmods):
+    mObj = Manifest(MANIFEST)
+    if sortmods == True:
+        mObj.manifest["files"] = sorted(mObj.mods(), key=lambda i: i["projectID"])
+    with open(MANIFEST_FILE, "w") as f:
+        json.dump(mObj.manifest, f, ensure_ascii=False)
+
+
 @ cli.command(help="根据Manifest的信息下载Mod")
 @ click.option("--download-dir", "-D", "download", default="", help="下载文件的路径,默认为本程序所在文件夹")
 @ click.option("--no-check", "no_check", is_flag=True, default=False, help="")
-@ click.option("--no-async", "no_async", is_flag=True, default=False, help="")
-@ click.option("--type", "type", default=0, help="Mod类型,0:忽略,1:仅服务端,2:仅客户端,3:双端")
-def dlmod(download, no_check, no_async, _type):
-    if no_check == False:
+@ click.option("--overwrite", is_flag=True, default=False, help="")
+# @ click.option("--no-async", "no_async", is_flag=True, default=False, help="")
+@ click.option("--type", "_type", default=0, help="Mod类型,0:忽略,1:仅服务端,2:仅客户端,3:双端")
+def dlmod(download, no_check, overwrite, _type):
+    if no_check == True:
         logging.info("命令行参数指定不进行Hash校验")
 
     if os.path.exists(download):
@@ -112,39 +128,50 @@ def dlmod(download, no_check, no_async, _type):
         logging.warning("指定的路径不可用或未指定路径,自动选择默认路径")
 
     mod_list = Manifest(MANIFEST).mods()
+    logging.info(f"共找到 {len(mod_list)} 个Mod, 开始下载任务")
     for index, item in enumerate(mod_list):
         if _type != 0:
             if "type" in item.keys():
                 if _type != item["type"]:
-                    break
+                    continue
         try:
             urllib3.disable_warnings()
             headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36"}
+
             r = requests.get(
                 f"{CONFIG['curseforge']['apiurl']}{item['projectID']}/file/{item['fileID']}",
                 headers=headers, verify=False, proxies=CONFIG["proxy"], timeout=5
-            ).text
-            download_url = json.loads(r)["downloadUrl"]
-            file_name = json.loads(r)["fileName"]
+            )
+            r.raise_for_status()
+            res_text = json.loads(r.text)
+            download_url = res_text["downloadUrl"]
+            file_name = res_text["fileName"]
+
             down_res = requests.get(download_url, headers=headers, proxies=CONFIG["proxy"], verify=False, timeout=120)
+            down_res.raise_for_status()
+            md5 = down_res.headers.get("ETag").replace("\"", "")
+
             file_path = f"{dpath}\{file_name}"
+            if os.path.exists(file_path):
+                logging.warning(f"[{index+1}/{len(mod_list)}] {file_name} 文件已存在 -> {('跳过') if overwrite == False else ('覆盖')}")
+                if overwrite == False:
+                    continue
             with open(file_path, "wb") as f:
                 f.write(down_res.content)
-            logging.info(f"[{index+1}/{len(mod_list)}] {file_name}下载完成")
+            logging.info(f"[{index+1}/{len(mod_list)}] {file_name} 下载完成")
         except Exception as e:
-            logging.error(f"在下载第{index+1}个mod时发生错误")
+            logging.error(f"[{index+1}/{len(mod_list)}] 错误: {e}")
             logging.debug(e)
-            break
+            continue
 
         if no_check == False:
             with open(file_path, "rb") as f:
-                sha1Obj = hashlib.sha1()
-                sha1Obj.update(f.read())
-                if "hash" in item.keys():
-                    if sha1Obj.hexdigest().upper().lower() != item["hash"].lower():
-                        logging.warning(f"所下载的第{index+1}个Mod{file_name}与Manifest中指定的Hash值不一致")
-                else:
-                    logging.warning(f"Manifest中的第{index+1}个Mod{file_name}未指定Hash值, 跳过校验")
+                md5Obj = hashlib.md5()
+                md5Obj.update(f.read())
+                _cmd5 = md5Obj.hexdigest().upper().lower()
+                logging.debug(_cmd5+" -> "+md5)
+                if _cmd5 != md5:
+                    logging.error(f"[{index+1}/{len(mod_list)}] {file_name} 校验失败: {_cmd5} != {md5}")
 
 
 if __name__ == "__main__":
