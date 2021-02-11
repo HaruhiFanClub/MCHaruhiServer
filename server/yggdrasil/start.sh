2846aa86dc148b626bd114476c8a57a40f295237
WakelessSloth56 <wsloth56@foxmail.com>
2021-02-11 11:01:43 +0800

    refactor: YggdrasilOfficialProxy

--- server/yggdrasil/start.sh
+++ server/yggdrasil/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cd `dirname $0`
-/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64/bin/java -Xmx512M -jar YggdrasilOfficialProxy-2.1.3-proxy.jar &
+cd $(dirname $0)
+while true
+do
+java -Xmx512M -jar YggdrasilOfficialProxy-2.1.3-proxy.jar
+sleep 15s
+done
