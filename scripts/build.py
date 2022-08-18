58aa6a54d54a21f7fea2042aecca12dfeebf5c33
WakelessSloth56 <wsloth56@foxmail.com>
2022-08-18 23:05:53 +0800

    build: data and resource packs
    [skip ci]

--- scripts/build.py
+++ scripts/build.py
@@ -19,14 +19,15 @@ TEMP_DIR = utils.getTempDir()
 BUILD_DIR = utils.getDir(os.path.abspath("build"))
 
 
-def buildPack(name: str):
+def buildPack(name: str, temp: bool = False):
     print(f"[Pack] Building pack: {name}")
-    zipFile = Pack(os.path.abspath(f"packs/{name}"), f"{ID}-{name}pack", VERSION).build(TEMP_DIR)
+    zipFile = Pack(os.path.abspath(f"packs/{name}"), f"{ID}-{name}pack", VERSION).build(TEMP_DIR if temp else BUILD_DIR)
     print(f"[Pack] Build complete: '{zipFile}'")
     return zipFile
 
 
 if __name__ == "__main__":
+    buildPack("data")
     modpack = Modpack(os.path.abspath("modpack"), "HaruhiServerModpack", VERSION)
-    # modpack.addResourcepack(buildPack("resource"))
+    modpack.addResourcepack(buildPack("resource", True))
     modpack.build(BUILD_DIR)
